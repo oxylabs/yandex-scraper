06c0ca4aa4cdf9f9494c9c127fb1dcb669564142
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2023-12-15 10:17:33 +0200

    Create yandex_curl.sh

---
+++ code examples/yandex_curl.sh
@@ -0,0 +1,4 @@
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "yandex", "url": "https://yandex.com/search/?text=nike&"}'
